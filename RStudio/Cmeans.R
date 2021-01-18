849af8d976a182621c7c1f026bf56f9a7c01822f
zanuarts <nuartea27@gmail.com>
2021-01-18 23:02:19 +0700

    fix clustering

--- RStudio/Cmeans.R
+++ RStudio/Cmeans.R
@@ -3,6 +3,6 @@ View(data)
 data.features = data
 View(data.features)
 library(e1071)
-hasil <- cmeans(data.features, 4, 2000, verbose=TRUE, method="cmeans", m=2)
+hasil <- cmeans(data.features, 3, 20, verbose=TRUE, method="cmeans", m=2)
 hasil
 plot(data[c('BALANCE', 'PURCHASES', 'ONEOFF_PURCHASES', 'CREDIT_LIMIT',	'PAYMENTS',	'MINIMUM_PAYMENTS')], col=hasil$cluster)
